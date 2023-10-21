610a5820f168300a30588c58eb6da477a65f2d1a
Sabuh <mastersabuh@gmail.com>
2023-10-21 15:11:23 +0200

    #86bw57erk basic Revertable system for power-ups

--- src/main/scala/org/aas/sbtanks/entities/powerups/PowerUp.scala
+++ src/main/scala/org/aas/sbtanks/entities/powerups/PowerUp.scala
@@ -10,14 +10,29 @@ object PowerUp:
 
     trait PowerUp[E]:
         def apply(entity: E): E
+        def revert(entity: E): E
 
 
-    trait PowerUpConstraint[E](predicate: E => Boolean) extends PowerUp[E]:
+
+    trait PowerUpConstraint[E](predicate: E => Boolean, reversePredicate: E => Boolean) extends PowerUp[E]:
         abstract override def apply(entity: E): E = Some(entity).filter(predicate).map(super.apply).getOrElse(entity)
 
+        abstract override def revert(entity: E): E =  Some(entity).filter(predicate).map(super.apply).getOrElse(entity)
 
-    case class FuncPowerUp[E](f: E => E) extends PowerUp[E]:
+
+
+    /*
+    trait CombinablePowerUp[E] extends PowerUp[E]:
+        def combine(other: CombinablePowerUp[E]): CombinablePowerUp[E] = CombinablePowerUp.combine(this, other)
+    */
+
+    //trait PowerUpTank[E <: Tank] extends PowerUpConstraint[E](entity => entity.isInstanceOf[Tank]):
+
+
+    case class FuncPowerUp[E](f: E => E, g: E => E) extends PowerUp[E]:
         override def apply(entity: E): E = f(entity)
+        override def revert(entity: E): E = g(entity)
+
 
 
     object PowerUp extends App:
--- src/main/scala/org/aas/sbtanks/entities/powerups/PowerUpChain.scala
+++ src/main/scala/org/aas/sbtanks/entities/powerups/PowerUpChain.scala
@@ -7,15 +7,29 @@ import org.aas.sbtanks.entities.tank.structure.*
 import org.aas.sbtanks.entities.tank.structure.Tank.BasicTank
 
 class PowerUpChain[E](powerUps: Seq[PowerUp[E]]) extends PowerUp[E]:
-    def apply(powerUps: PowerUp[E]*) =
+    def apply(powerUps: PowerUp[E]*): PowerUpChain[E] =
         PowerUpChain(powerUps)
 
     override def apply(entity: E): E =
         powerUps.foldLeft(entity)((acc, powerUp) => powerUp.apply(acc))
 
+    override def revert(entity: E): E =
+        powerUps.foldRight(entity)((powerUp, acc) => powerUp.revert(acc))
 
-    def combine(next: PowerUp[E]): PowerUpChain[E] = PowerUpChain(powerUps :+ next)
 
+    def chain(next: PowerUp[E]): PowerUpChain[E] = PowerUpChain(powerUps :+ next)
+
+    def unchain(last: PowerUp[E]): PowerUpChain[E] = PowerUpChain(powerUps.filter(_ != last))
+
+    //def unchain(last: PowerUp[E]): PowerUpChain[E] = PowerUpChain(powerUps.filter(_ != last))
+
+/*
+trait PowerUpDispatcher[E, P <: PowerUp[E]]:
+    def subscribe(entity: E): Unit
+    def unsubscribe(entity: E): Unit
+
+    def publish(next: PowerUp[E]): P
+*/
 
 
 class DispatchablePowerUpChain[E] extends PowerUpChain[E](Seq.empty):
@@ -33,10 +47,17 @@ class DispatchablePowerUpChain[E] extends PowerUpChain[E](Seq.empty):
         entities = entities.collect:
             case e if e.supplier() != entity => e
 
+    override def chain(next: PowerUp[E]): PowerUpChain[E] =
+        entities.foreach(e => e.consumer(
+            next(e.supplier()))
+        )
+        super.chain(next)
 
-    override def combine(next: PowerUp[E]): PowerUpChain[E] =
-        entities.foreach(e => e.consumer(next(e.supplier())))
-        super.combine(next)
+    override def unchain(last: PowerUp[E]): PowerUpChain[E] =
+        entities.foreach(e => e.consumer(
+            last.revert(e.supplier()))
+        )
+        super.unchain(last)
 
 
 
@@ -50,12 +71,21 @@ object PowerUpChain extends App:
     val tank = BasicTank()
     val tank2 = BasicTank()
 
+     val healthUp: PowerUp[Tank] = FuncPowerUp(
+        t => {t updateTankData (
+            t.tankData.updateHealth(_ + 10)
+        ); t},
+          t => {t updateTankData (
+                t.tankData.updateHealth(_ - 10)
+            ); t}
+    )
+
     persChainer.subscribe(tank)
 
-    persChainer.combine(FuncPowerUp(t => {t updateTankData (
-        t.tankData.updateHealth(_ + 10)
-        ); t}   ))
 
+    persChainer.chain(healthUp)
+
+    persChainer.unchain(healthUp)
 
 
     persChainer.subscribe(tank2)
@@ -66,11 +96,8 @@ object PowerUpChain extends App:
     println(tank.tankData.health)
 
     persChainer.unsubscribe(tank)
-    persChainer.unsubscribe(tank2)
 
-    persChainer.combine(FuncPowerUp(t => {t updateTankData (
-        t.tankData.updateHealth(_ + 10)
-        ); t}   ))
+    persChainer.chain(healthUp)
 
     println(tank2.tankData.health)
     println(tank.tankData.health)
