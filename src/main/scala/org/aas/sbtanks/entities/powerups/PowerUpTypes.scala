d89fed7758545c0f9ecd1492aa0dfbb14a28a3cd
Sabuh <mastersabuh@gmail.com>
2023-10-23 11:00:58 +0200

    #86bw5734b added HealthUp and SpeedUp powerUp template

---
+++ src/main/scala/org/aas/sbtanks/entities/powerups/PowerUpTypes.scala
@@ -0,0 +1,33 @@
+package org.aas.sbtanks.entities.powerups
+
+import org.aas.sbtanks.entities.powerups.PowerUp.{FuncPowerUp, PowerUpConstraint}
+import org.aas.sbtanks.entities.tank.structure.Tank
+import org.aas.sbtanks.entities.tank.structure.Tank.BasicTank
+
+
+//abstract class PowerUpTank[E <: Tank] extends PowerUpConstraint[E](entity => entity.isInstanceOf[Tank], _ => true):
+
+
+//TODO: refactor function to apply and constraint condition for each power-up
+
+val h: Tank => Tank = t => {t updateTankData(t.tankData.updateHealth(_ + 10)); t}
+val h2: Tank => Tank = t => {t updateTankData(t.tankData.updateHealth(_ - 10)); t}
+
+object HealthUp extends FuncPowerUp[Tank](h, h2) with PowerUpConstraint[Tank](entity => entity.isInstanceOf[Tank], _ => true)
+
+val s: Tank => Tank = t => {t updateTankData(t.tankData.updateSpeed(_ + 10)); t}
+val s2: Tank => Tank = t => {t updateTankData(t.tankData.updateSpeed(_ - 10)); t}
+
+object SpeedUp extends FuncPowerUp[Tank](s, s2) with PowerUpConstraint[Tank](entity => entity.isInstanceOf[Tank], _ => true)
+
+
+
+
+
+
+
+
+
+
+
+
