9381df1d71189311014808291167a001c3d1bd3f
Sabuh <mastersabuh@gmail.com>
2023-10-26 16:06:21 +0200

    #86bw88974 PowerUpView now extend ImageView

--- src/main/scala/org/aas/sbtanks/entities/powerups/view/PowerUpView.scala
+++ src/main/scala/org/aas/sbtanks/entities/powerups/view/PowerUpView.scala
@@ -3,13 +3,57 @@ package org.aas.sbtanks.entities.powerups.view
 import scalafx.animation.{KeyFrame, Timeline}
 import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
-import scalafx.beans.property.BooleanProperty
+import scalafx.beans.property.{BooleanProperty, ObjectProperty}
 import scalafx.scene.Scene
 import scalafx.scene.image.{Image, ImageView}
 import scalafx.util.Duration
 
+import javafx.scene.{image => jfxsi}
 
 
+
+
+class PowerUpView(private val image: Image) extends jfxsi.ImageView(image):
+
+    private val isVisibleOnScene: BooleanProperty = BooleanProperty(true)
+
+    this.visibleProperty().bind(isVisibleOnScene)
+
+
+    private val timeLine: Timeline = new Timeline:
+        keyFrames = Seq(
+            KeyFrame(Duration(500), onFinished = _ => isVisibleOnScene.value = !isVisibleOnScene.value)
+        )
+        cycleCount = 10
+        onFinished = _ => isVisibleOnScene.value = false
+
+    def startAnimation(): Unit =
+        timeLine.play()
+
+object PowerUpView:
+    def apply(imagePath: String): PowerUpView =
+        new PowerUpView(Image(imagePath))
+
+
+
+
+object PowerUpViewTest extends JFXApp3:
+
+
+    override def start(): Unit =
+        val view =  PowerUpView("entities/powerups/powerup_star.png")
+
+        view.startAnimation()
+
+        stage = new PrimaryStage:
+            title = "Power-Up View"
+            scene = new Scene:
+                content = ImageView(view)
+
+
+
+
+/*
 object PowerUpView extends JFXApp3:
 
     override def start(): Unit =
@@ -37,3 +81,4 @@ object PowerUpView extends JFXApp3:
             title = "Power-Up View"
             scene = new Scene:
                 content = powerUpImageView
+*/
