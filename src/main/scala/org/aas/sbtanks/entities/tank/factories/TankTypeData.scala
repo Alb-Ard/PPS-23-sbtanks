f21001376924c0b997192043e598840353dabbd0
Sabuh <mastersabuh@gmail.com>
2023-10-28 11:17:12 +0200

    #86bw5667f small addition in tank data structure

--- src/main/scala/org/aas/sbtanks/entities/tank/factories/TankTypeData.scala
+++ src/main/scala/org/aas/sbtanks/entities/tank/factories/TankTypeData.scala
@@ -13,18 +13,18 @@ trait TankTypeData:
 
 
 case object BasicTankData extends TankTypeData:
-    override def supplyData = new TankData(1, 1) with TankDataUpdater
+    override def supplyData = new TankData(1, 1, 1) with TankDataUpdater
 
 case object FastTankData extends TankTypeData:
-  override def supplyData = new TankData(1, 3) with TankDataUpdater
+  override def supplyData = new TankData(1, 3, 2) with TankDataUpdater
 
 
 case object PowerTankData extends TankTypeData:
-  override def supplyData = new TankData(1, 2) with TankDataUpdater
+  override def supplyData = new TankData(1, 2, 3) with TankDataUpdater
 
 
 case object ArmorTankData extends TankTypeData:
-  override def supplyData = new TankData(4, 2) with TankDataUpdater
+  override def supplyData = new TankData(4, 2, 2) with TankDataUpdater
 
 
 
