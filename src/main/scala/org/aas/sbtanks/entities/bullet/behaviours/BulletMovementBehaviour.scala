0593113ab773a1814636c448bfa37536c524ddcb
Andrea Bianchi <andrea.bianchi24@studio.unibo.it>
2023-10-28 10:54:25 +0200

    minor changes in TankShooting and BulletMovementBehaviour

--- src/main/scala/org/aas/sbtanks/entities/bullet/behaviours/BulletMovementBehaviour.scala
+++ src/main/scala/org/aas/sbtanks/entities/bullet/behaviours/BulletMovementBehaviour.scala
@@ -2,13 +2,18 @@ package org.aas.sbtanks.entities.bullet.behaviours
 
 //import java.util.Calendar
 import org.aas.sbtanks.entities.bullet.Bullet
+import org.aas.sbtanks.behaviours.SteppedMovementDirectionBehaviour
+import org.aas.sbtanks.behaviours.CollisionBehaviour
 
-trait BulletMovementBehaviour(bullet: Bullet):
+trait BulletMovementBehaviour(bullet: Bullet with SteppedMovementDirectionBehaviour) extends Bullet:
 
     //val now: Int = Calendar.getInstance().get(Calendar.SECOND)
 
+    def checkCollision: Boolean =
+
+
     def move(): Bullet =
-      Thread.sleep(2000);
+      //Thread.sleep(2000);
       bullet.bulletPosition = (bullet.bulletPosition._1 +
                                       (bullet.bulletDirection._1 * bullet.speed),
                                       bullet.bulletPosition._2 +
--- src/main/scala/org/aas/sbtanks/entities/tank/behaviours/TankShooting.scala
+++ src/main/scala/org/aas/sbtanks/entities/tank/behaviours/TankShooting.scala
@@ -1,12 +1,15 @@
 package org.aas.sbtanks.entities.tank.behaviours
 
 import org.aas.sbtanks.entities.bullet.Bullet
-import org.aas.sbtanks.entities.bullet.behaviours.BulletMovementBehaviour
+import org.aas.sbtanks.behaviours.{PositionBehaviour, SteppedMovementDirectionBehaviour}
 
-trait TankShootingBehaviour(speedData: Int, moveInfo: (Double, Double),
+//trait TankShootingBehaviour(speedData: Int, moveInfo: (Double, Double),
+//                                posX: Double, posY: Double):
+trait TankShootingBehaviour(speedData: Int, direction: SteppedMovementDirectionBehaviour,
                             posX: Double, posY: Double):
+    self: PositionBehaviour with SteppedMovementDirectionBehaviour
 
     private var position: (Double, Double) = (posX, posY)
 
-    def shoot(): Bullet =
-        Bullet(speedData, moveInfo, position)
+    def shoot(): Bullet with SteppedMovementDirectionBehaviour =
+        Bullet(speedData, direction.direction, position)  with direction
