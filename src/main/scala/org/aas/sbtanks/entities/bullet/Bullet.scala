962401ca8ab13e220f2e0f8346d4cba264ac08a1
Andrea Bianchi <andrea.bianchi24@studio.unibo.it>
2023-10-30 09:01:55 +0100

    refactor in case class Bullet

--- src/main/scala/org/aas/sbtanks/entities/bullet/Bullet.scala
+++ src/main/scala/org/aas/sbtanks/entities/bullet/Bullet.scala
@@ -6,7 +6,4 @@ val bulletSpeed: Int = speed
 val bulletDirection: (Double, Double) = direction
 var bulletPosition:(Double, Double) = (position._1 + bulletDirection._1, position._2 + bulletDirection._2)
  */
-case class Bullet(speed: Int, playerBullet: Boolean):
-
-    val bulletSpeed:Int = speed
-    val isPlayerBullet: Boolean = playerBullet
+case class Bullet(val speed: Int, val isPlayerBullet: Boolean)
