6c688754a5ffe39f81ee807eb20c3218daf7d800
Andrea Bianchi <andrea.bianchi24@studio.unibo.it>
2023-10-30 10:59:09 +0100

    removed comments and very minor changes in JFXBulletView, added system to identify collisions and certain actions that will be done when given a certain collider

--- src/main/scala/org/aas/sbtanks/entities/bullet/controller/BulletController.scala
+++ src/main/scala/org/aas/sbtanks/entities/bullet/controller/BulletController.scala
@@ -4,13 +4,14 @@ import org.aas.sbtanks.entities.bullet.Bullet
 import org.aas.sbtanks.entities.tank.structure.Tank
 import org.aas.sbtanks.obstacles.LevelObstacle
 import org.aas.sbtanks.behaviours.{CollisionBehaviour, DirectionBehaviour, PositionBehaviour}
-import org.aas.sbtanks.physics.{Collider, CollisionLayer}
+import org.aas.sbtanks.physics.{Collider, CollisionLayer, PhysicsWorld}
 import org.aas.sbtanks.common.Steppable
+import org.aas.sbtanks.entities.tank.{TankData, TankDataUpdater}
 
 trait BulletController(bullet: Bullet with PositionBehaviour with DirectionBehaviour with CollisionBehaviour) extends Steppable :
 
     override def step(delta: Double): Steppable =
-        checkCollision()
+        checkCollision() //non serve passare un collider perchè il Bullet è un Collider!
         bullet.positionChanged((bullet.positionX + (bullet.directionX * bullet.speed),
             bullet.positionY + (bullet.directionY * bullet.speed)))
         this
@@ -19,8 +20,17 @@ trait BulletController(bullet: Bullet with PositionBehaviour with DirectionBehav
     //bullet tank colpisce altri bullets di tutti i tipi, ostacoli e il player
     //bullet player colpisce gli altri tank, gli ostacoli e i proiettili di tutti i tipi
     private def checkCollision(): Unit =
-        if(bullet.overlapsAnything) then
+        if(bullet.overlapsAnything)
             val collider = bullet.overlappedColliders.filter(el => el.layer == CollisionLayer.BulletsLayer ||
                                                 el.layer == CollisionLayer.WallsLayer || el.layer == CollisionLayer.TanksLayer)
-            //if(collider.length == 1 && collider.contains(CollisionLayer.BulletsLayer))
+            if(collider.contains(CollisionLayer.TanksLayer))
+                val hitEnemy: Tank = collider.find(el => el.layer == CollisionLayer.TanksLayer).get.asInstanceOf[Tank]
+                hitEnemy.updateTankData(hitEnemy.tankData.updateHealth(_ - 1))
+            if(collider.contains(CollisionLayer.WallsLayer))
+                val hitWall = collider.find(el => el.layer == CollisionLayer.WallsLayer).get.asInstanceOf[LevelObstacle]
+            if(collider.contains(CollisionLayer.BulletsLayer))
+                val hitBullet: Bullet = collider.find(el => el.layer == CollisionLayer.BulletsLayer).get.asInstanceOf[Bullet]
+                //elimina qui il proiettile con cui mi sono scontrato
+            //PhysicsWorld.unregisterCollider(bullet)
+
 
--- src/main/scala/org/aas/sbtanks/entities/bullet/view/JFXBulletView.scala
+++ src/main/scala/org/aas/sbtanks/entities/bullet/view/JFXBulletView.scala
@@ -1,18 +1,16 @@
 package org.aas.sbtanks.entities.bullet.view
 
 import javafx.animation.Animation
-import org.aas.sbtanks.common.view.JFXMoveableView
-import org.aas.sbtanks.common.view.JFXDirectionableView
+import org.aas.sbtanks.common.view.MoveableView
+import org.aas.sbtanks.common.view.DirectionableView
+import org.aas.sbtanks.resources.scalafx.JFXImageLoader
 import scalafx.scene.image.ImageView
 import scalafx.scene.Scene
 import scalafx.scene.shape.Rectangle
 import scalafx.scene.Node
 import scalafx.scene.image.Image
 
-//class JFXBulletView(image: Image, animationSpeed: Double = 1) extends ImageView with JFXMoveableView with JFXDirectionableView:
+class JFXBulletView(image: Image, animationSpeed: Double = 1) extends ImageView with MovableView with DirectionableView with JFXImageLoader:
 
-    /*
-    val animation = Animation { now =>
-        image = (now * animationSpeed)
-    }*/
+    ImageView.sfxImageView2jfx().setImage(JFXImageLoader.loadFromResources(1, 1))
 
