8c028e7777dfc0f03dc6803308302d110e2920af
Alberto Arduini <alberto.arduini3@studio.unibo.it>
2023-10-31 01:04:35 +0100

    Set event source to use weak refs to callbacks

--- src/main/scala/org/aas/sbtanks/event/EventSource.scala
+++ src/main/scala/org/aas/sbtanks/event/EventSource.scala
@@ -1,18 +1,20 @@
 package org.aas.sbtanks.event
 
+import scala.ref.WeakReference
+
 class EventSource[A]:
     type EventCallback = A => Any
 
-    private var listeners = List.empty[EventCallback]
+    private var listeners = List.empty[WeakReference[EventCallback]]
 
     def += (callback: EventCallback) = 
-        listeners = callback :: listeners
+        listeners = WeakReference(callback) :: listeners
 
     def -= (callback: EventCallback) = 
-        listeners = listeners.filterNot(c => c == callback)
+        listeners = listeners.filterNot(c => c.get forall { oc => oc == callback })
 
     def apply(param: A): Unit =
         invoke(param)
 
     def invoke(param: A): Unit =
-        listeners foreach { p => p(param) }
+        listeners foreach { c => c.get foreach { oc => oc(param) } }
