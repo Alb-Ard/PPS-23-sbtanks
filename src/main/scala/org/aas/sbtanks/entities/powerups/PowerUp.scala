3632c737ac2eb73a4dd13f9d19e6e4824c91d421
Sabuh <mastersabuh@gmail.com>
2023-11-02 10:00:02 +0100

    fix in power-up structure

--- src/main/scala/org/aas/sbtanks/entities/powerups/PowerUp.scala
+++ src/main/scala/org/aas/sbtanks/entities/powerups/PowerUp.scala
@@ -8,7 +8,7 @@ import org.aas.sbtanks.entities.tank.structure.Tank.BasicTank
 
 object PowerUp:
 
-    trait PowerUp[E]:
+    abstract class PowerUp[E]:
         def apply(entity: E): E
         def revert(entity: E): E
 
@@ -18,10 +18,9 @@ object PowerUp:
 
 
 
-    trait PowerUpConstraint[E](predicate: E => Boolean, reversePredicate: E => Boolean) extends PowerUp[E]:
+    trait PowerUpConstraint[E](predicate: E => Boolean) extends PowerUp[E]:
         abstract override def apply(entity: E): E = Some(entity).filter(predicate).map(super.apply).getOrElse(entity)
 
-        abstract override def revert(entity: E): E =  Some(entity).filter(reversePredicate).map(super.revert).getOrElse(entity)
 
 
 
@@ -29,7 +28,7 @@ object PowerUp:
 
 
 
-    case class FuncPowerUp[E](f: E => E, g: E => E) extends PowerUp[E]:
+    trait FuncPowerUp[E](f: E => E, g: E => E) extends PowerUp[E]:
         override def apply(entity: E): E = f(entity)
         override def revert(entity: E): E = g(entity)
 
--- src/main/scala/org/aas/sbtanks/entities/powerups/PowerUpTypes.scala
+++ src/main/scala/org/aas/sbtanks/entities/powerups/PowerUpTypes.scala
@@ -13,18 +13,18 @@ import org.aas.sbtanks.entities.tank.structure.Tank.BasicTank
 val h: Tank => Tank = t => {t updateTankData(t.tankData.updateHealth(_ + 10)); t}
 val h2: Tank => Tank = t => {t updateTankData(t.tankData.updateHealth(_ - 10)); t}
 
-object HealthUp extends FuncPowerUp[Tank](h, h2) with PowerUpConstraint[Tank](entity => entity.isInstanceOf[Tank], _ => true)
+object HealthUp extends FuncPowerUp[Tank](h, h2) with PowerUpConstraint[Tank](entity => entity.isInstanceOf[Tank])
 
 val s: Tank => Tank = t => {t updateTankData(t.tankData.updateSpeed(_ + 10)); t}
 val s2: Tank => Tank = t => {t updateTankData(t.tankData.updateSpeed(_ - 10)); t}
 
-object SpeedUp extends FuncPowerUp[Tank](s, s2) with PowerUpConstraint[Tank](entity => entity.isInstanceOf[Tank], _ => true)
+object SpeedUp extends FuncPowerUp[Tank](s, s2) with PowerUpConstraint[Tank](entity => entity.isInstanceOf[Tank])
 
 
 val sb: Tank => Tank = t => {t updateTankData(t.tankData.updateBulletSpeed(_ + 10)); t}
 val sb2: Tank => Tank = t => {t updateTankData(t.tankData.updateBulletSpeed(_ - 10)); t}
 
-object SpeedBulletUp extends FuncPowerUp[Tank](sb, sb2) with PowerUpConstraint[Tank](entity => entity.isInstanceOf[Tank], _ => true)
+object SpeedBulletUp extends FuncPowerUp[Tank](sb, sb2) with PowerUpConstraint[Tank](entity => entity.isInstanceOf[Tank])
     
 
 object a extends App:
