bb7f68242b2eb9d5daee42004cdb4c5519a61590
Andrea Bianchi <andrea.bianchi24@studio.unibo.it>
2023-11-03 17:27:24 +0100

    slight changes in TankShooting so that it generates a damageable/destroyable bullet, changes in BulletController so that when a tank is hit, it takes damage and gets destroyed when it has no health. This also destroys bullets

--- src/main/scala/org/aas/sbtanks/entities/bullet/controller/BulletController.scala
+++ src/main/scala/org/aas/sbtanks/entities/bullet/controller/BulletController.scala
@@ -3,12 +3,12 @@ package org.aas.sbtanks.entities.bullet.controller
 import org.aas.sbtanks.entities.bullet.Bullet
 import org.aas.sbtanks.entities.tank.structure.Tank
 import org.aas.sbtanks.obstacles.LevelObstacle
-import org.aas.sbtanks.behaviours.{CollisionBehaviour, DirectionBehaviour, PositionBehaviour}
+import org.aas.sbtanks.behaviours.{CollisionBehaviour, DamageableBehaviour, DirectionBehaviour, PositionBehaviour}
 import org.aas.sbtanks.physics.{Collider, CollisionLayer, PhysicsWorld}
 import org.aas.sbtanks.common.Steppable
 import org.aas.sbtanks.entities.tank.{TankData, TankDataUpdater}
 
-trait BulletController(bullet: Bullet with PositionBehaviour with DirectionBehaviour with CollisionBehaviour) extends Steppable :
+trait BulletController(bullet: Bullet with PositionBehaviour with DirectionBehaviour with CollisionBehaviour with DamageableBehaviour) extends Steppable :
 
     override def step(delta: Double): Steppable =
         checkCollision() //non serve passare un collider perchè il Bullet è un Collider!
@@ -24,12 +24,17 @@ trait BulletController(bullet: Bullet with PositionBehaviour with DirectionBehav
             val collider = bullet.overlappedColliders.filter(el => el.layer == CollisionLayer.BulletsLayer ||
                                                 el.layer == CollisionLayer.WallsLayer || el.layer == CollisionLayer.TanksLayer)
             if(collider.contains(CollisionLayer.TanksLayer))
-                val hitEnemy: Tank = collider.find(el => el.layer == CollisionLayer.TanksLayer).get.asInstanceOf[Tank]
-                hitEnemy.updateTankData(hitEnemy.tankData.updateHealth(_ - 1))
+                val hitEnemy = collider.find(el => el.layer == CollisionLayer.TanksLayer).get.asInstanceOf[Tank with DamageableBehaviour]
+                hitEnemy.damage()
+                if(hitEnemy.tankData.health <= 0)
+                    hitEnemy.destroyed
+                //hitEnemy.updateTankData(hitEnemy.tankData.updateHealth(_ - 1))
             if(collider.contains(CollisionLayer.WallsLayer))
                 val hitWall = collider.find(el => el.layer == CollisionLayer.WallsLayer).get.asInstanceOf[LevelObstacle]
             if(collider.contains(CollisionLayer.BulletsLayer))
-                val hitBullet: Bullet = collider.find(el => el.layer == CollisionLayer.BulletsLayer).get.asInstanceOf[Bullet]
+                val hitBullet= collider.find(el => el.layer == CollisionLayer.BulletsLayer).get.asInstanceOf[Bullet with DamageableBehaviour]
+                bullet.destroyed
+                hitBullet.destroyed
                 //elimina qui il proiettile con cui mi sono scontrato
             //PhysicsWorld.unregisterCollider(bullet)
 
