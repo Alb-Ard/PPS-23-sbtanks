f1c7f5401b27d8bb18c436336222663bef467430
Andrea Bianchi <andrea.bianchi24@studio.unibo.it>
2023-11-08 12:38:38 +0100

    removed comments and unused imports

--- src/main/scala/org/aas/sbtanks/entities/bullet/view/scalafx/JFXBulletView.scala
+++ src/main/scala/org/aas/sbtanks/entities/bullet/view/scalafx/JFXBulletView.scala
@@ -1,13 +1,9 @@
 package org.aas.sbtanks.entities.bullet.view.scalafx
 
-import org.aas.sbtanks.behaviours.{CollisionBehaviour, DamageableBehaviour, DirectionBehaviour, PositionBehaviour}
-import org.aas.sbtanks.common.view.scalafx.JFXImageViewAnimator
 import org.aas.sbtanks.common.view.{JFXDirectionableView, JFXMoveableView}
 import org.aas.sbtanks.entities.bullet.view.BulletView
 import scalafx.Includes
 import scalafx.scene.image.{Image, ImageView}
-import org.aas.sbtanks.entities.bullet.controller.BulletController
-import org.aas.sbtanks.entities.bullet.Bullet
 
 class JFXBulletView(private val bulletImage: Seq[Image], animationSpeed: Double = 1) extends ImageView
     with BulletView
@@ -18,14 +14,4 @@ class JFXBulletView(private val bulletImage: Seq[Image], animationSpeed: Double 
      */
     with JFXDirectionableView
     with JFXMoveableView[ImageView]
-    with JFXImageViewAnimator(bulletImage, animationSpeed)
-
-    /*
-    private val rotationValue = 90D
-    override def look(rotation: Double): Unit =
-        setImages(Seq(bulletImage((rotation / rotationValue).toInt)))
-    override def move(x: Double, y: Double): Unit = super.move(x, y)
-     */
-
-    //ImageView.sfxImageView2jfx().setImage(JFXImageLoader.loadFromResources(1, 1))
 
