8ead269ac9454911dd325534a40d17ba12d72151
Sabuh <mastersabuh@gmail.com>
2023-11-10 17:29:34 +0100

    #86bw9mefx small fix in AiEntity typing

--- src/main/scala/org/aas/sbtanks/enemies/controller/EnemyController.scala
+++ src/main/scala/org/aas/sbtanks/enemies/controller/EnemyController.scala
@@ -8,7 +8,7 @@ import org.aas.sbtanks.enemies.ai.EnemyUtils.Enemy
 import org.aas.sbtanks.entities.tank.controller.TankController.ControllableTank
 import org.aas.sbtanks.enemies.controller.AiMovableController
 
-class EnemyController(private val enemyTanks: Seq[AiEntity]) extends AiMovableController(enemyTanks) with Steppable:
+class EnemyController(private val enemyTanks: Seq[ControllableTank]) extends AiMovableController(enemyTanks.map(_.asInstanceOf[AiEntity])) with Steppable:
 
     override def step(delta: Double) =
         this.computeStates()
