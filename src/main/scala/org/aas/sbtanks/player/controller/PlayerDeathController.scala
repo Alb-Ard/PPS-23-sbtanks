51ed546f322fce54fa8f3b7a03d39a93253828aa
Andrea Bianchi <andrea.bianchi24@studio.unibo.it>
2023-11-13 00:02:12 +0100

    mockup of respawn method

--- src/main/scala/org/aas/sbtanks/player/controller/PlayerDeathController.scala
+++ src/main/scala/org/aas/sbtanks/player/controller/PlayerDeathController.scala
@@ -1,5 +1,11 @@
 package org.aas.sbtanks.player.controller
 
-import org.aas.sbtanks.player.PlayerTank
-class PlayerDeathController(player: PlayerTank)
+import org.aas.sbtanks.player.{PlayerTank, PlayerTankBuilder}
+class PlayerDeathController(player: PlayerTank):
+
+    def respawn(): PlayerTank =
+        val startingPosition = (0, 0)
+        PlayerTankBuilder()
+            .setPosition(startingPosition(0), startingPosition(1))
+            .build()
 
