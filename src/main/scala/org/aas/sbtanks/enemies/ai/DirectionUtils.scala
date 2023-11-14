d911f491e697aea84af65de0215b1a13e96eeba7
Sabuh <mastersabuh@gmail.com>
2023-11-14 21:17:54 +0100

    fixed typo in DirectionUtils; added controller factory

--- src/main/scala/org/aas/sbtanks/enemies/ai/DirectionUtils.scala
+++ src/main/scala/org/aas/sbtanks/enemies/ai/DirectionUtils.scala
@@ -9,6 +9,6 @@ object DirectionUtils:
 
     val Bottom_Y: (Double, Double) = (0.0, 1.0)
     val Right_X: (Double, Double) = (1.0, 0.0)
-    val Left_X: (Double, Double) = (0.0, -1.0)
-    val Top_Y: (Double, Double) = (-1.0, 0.0)
+    val Left_X: (Double, Double) = (-1.0, 0.0)
+    val Top_Y: (Double, Double) = (0.0, -1.0)
 
