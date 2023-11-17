ce3bdda9f6744f1c243b635c927b10b9dd1effa6
Sabuh <mastersabuh@gmail.com>
2023-11-18 00:13:08 +0100

    #86bw9meba added PlayerBase priority sight check; fixed seeThrough threshold

--- src/main/scala/org/aas/sbtanks/enemies/ai/shooting/LineOfSight.scala
+++ src/main/scala/org/aas/sbtanks/enemies/ai/shooting/LineOfSight.scala
@@ -1,32 +1,48 @@
 package org.aas.sbtanks.enemies.ai.shooting
 
 import org.aas.sbtanks.behaviours.{DirectionBehaviour, PositionBehaviour}
+import org.aas.sbtanks.enemies.ai.DirectionUtils.*
+import org.aas.sbtanks.enemies.ai.shooting.LineOfSight.getCollisionLines
+import org.aas.sbtanks.obstacles.LevelObstacle
+import org.aas.sbtanks.obstacles.LevelObstacle.PlayerBase
 import org.aas.sbtanks.physics.Raycast.*
 import org.aas.sbtanks.physics.{Collider, CollisionLayer, PhysicsContainer, PhysicsWorld}
 
-trait LineOfSight(private val priorityCollider: Seq[Collider], private val priorityTake: Int, private val lineCollisions: Seq[CollisionLayer], private val exclusion: Seq[Collider]):
+trait LineOfSight(private val lineCollisions: Seq[CollisionLayer], private val exclusion: Seq[Collider], private val seeThrough: Int = 5):
     this: PositionBehaviour with DirectionBehaviour =>
 
 
-    /*
-        TODO: check with the current direction, otherwise change it to the one with the major amount of priority colliders
-     */
+    def getCollidersOn(direction: Direction): Seq[Collider] =
+        getPriorityColliders(getCollisionLines(this)(direction))
 
-    def getPriorityCollisionLine: (Double, Double) =
-        if getPriorityColliders(getVerticalLine).size >= getPriorityColliders(getHorizzontalLine).size then (0,1) else (1,0)
-
-
-
-
-    private def getPriorityColliders(colliders: Seq[Collider]): Seq[Collider] = colliders.take(priorityTake).filter(priorityCollider.contains(_))
-
-    private def getVerticalLine: Seq[Collider] =
-        PhysicsWorld.verticalRayCast(this.positionX, this.positionY, Option.empty, lineCollisions, exclusion)
-
-    private def getHorizzontalLine: Seq[Collider] =
-        PhysicsWorld.horizontalRayCast(this.positionX, this.positionY, Option.empty, lineCollisions, exclusion)
+    private def getPriorityColliders(colliders: Seq[Collider]): Seq[Collider] =
+        colliders
+            .map(_.asInstanceOf[LevelObstacle with PositionBehaviour])
+            .zipWithIndex
+            .filter:
+                case (collider, index) =>
+                    PlayerBase(collider.positionX, collider.positionY).contains(collider) && index <= seeThrough
+            .map:
+                case (collider, _) =>
+                    collider.asInstanceOf[Collider]
 
 
+    private def getVerticalLine(backwards: Boolean = false): Seq[Collider] =
+        PhysicsWorld.verticalRayCast(this.positionX, this.positionY, if backwards then Some(-Int.MaxValue) else Option.empty, lineCollisions, exclusion)
+
+
+    private def getHorizontalLine(backwards: Boolean = false): Seq[Collider] =
+        PhysicsWorld.horizontalRayCast(this.positionX, this.positionY, if backwards then Some(-Int.MaxValue) else Option.empty, lineCollisions, exclusion)
+
+
+object LineOfSight {
+    def getCollisionLines(los: LineOfSight): Map[Direction, Seq[Collider]] = Map(
+        Bottom -> los.getVerticalLine(),
+        Top -> los.getVerticalLine(true),
+        Left -> los.getHorizontalLine(true),
+        Right -> los.getHorizontalLine()
+    )
+}
 
 
 
