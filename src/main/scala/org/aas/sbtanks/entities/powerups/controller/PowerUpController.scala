d83086a3ba7e04e1f97f36db31d2545377f0ddc1
Sabuh <mastersabuh@gmail.com>
2023-11-28 22:30:13 +0100

    86bwjk8f8 added disappear time to pickable powerups; added event for powerup pick-up

--- src/main/scala/org/aas/sbtanks/entities/powerups/controller/PowerUpController.scala
+++ src/main/scala/org/aas/sbtanks/entities/powerups/controller/PowerUpController.scala
@@ -7,40 +7,75 @@ import org.aas.sbtanks.entities.powerups.view.PowerUpView
 import org.aas.sbtanks.entities.repository.EntityMvRepositoryContainer
 import org.aas.sbtanks.entities.repository.context.EntityRepositoryContext
 import org.aas.sbtanks.entities.tank.structure.Tank
+import org.aas.sbtanks.event.EventSource
 import org.aas.sbtanks.obstacles.LevelObstacle
 import org.aas.sbtanks.physics.Collider
 import org.aas.sbtanks.player.PlayerTank
 import scalafx.scene.Node
 import scalafx.stage.Stage
 
-class PowerUpController[VSK, VS, E](using context: EntityRepositoryContext[Stage, VSK, VS])(entityRepo: EntityMvRepositoryContainer[AnyRef, Node], powerUp: PickablePowerUp[E], view: PowerUpView, binder: PowerUpChainBinder[E]) extends Steppable:
+/**
+ * A controller for managing the behavior of pickable power-ups in the game.
+ *
+ * @tparam E   The type of the entity associated with the power-up.
+ * @param context   The entity repository context providing access to the game stage.
+ * @param entityRepo The repository container for managing entities MVCs.
+ * @param powerUp    The pickable power-up instance associated with this controller.
+ * @param view       The view representing the power-up in the game.
+ * @param viewScale  The scale factor for the power-up view visualization.
+ */
+class PowerUpController[VSK, VS, E](using context: EntityRepositoryContext[Stage, VSK, VS])(entityRepo: EntityMvRepositoryContainer[AnyRef, Node], powerUp: PickablePowerUp[E], view: PowerUpView, viewScale: Double) extends Steppable:
+
+    /**
+     * An event source for notifying observers when a power-up is picked up.
+     */
+    private val pickup: EventSource[PowerUp[E]] = EventSource[PowerUp[E]]
+
+    private var timeToDisappear: Double = 5.0
+
+    /**
+     * Handles the overlapping collision event of the power-up with other entities.
+     * It's used to remove its own mv wether a powerup is picked-up by the player tank
+     *
+     * @param colliders The colliders with which the power-up is overlapping.
+     */
     powerUp.overlapping += { (colliders) =>
 
-        /*
-            TODO: do not manipulate directly binder, but send an event to main powerup controller
-         */
-        checkTankCollision(colliders).foreach
-            binder.chain(powerUp)
+        checkTankCollision(colliders).headOption
+            pickup(powerUp)
 
         entityRepo.removeModelView(powerUp)
 
     }
 
+    /**
+     * Checks for tank collision among a sequence of colliders.
+     *
+     * @param colliders The colliders to check for tank collision.
+     * @return A sequence of tanks that have collided with the power-up or an empty sequence if nothing is found.
+     */
     private def checkTankCollision(colliders: Seq[Collider]): Seq[Tank] =
         colliders.collect:
             case element: Tank if element.isInstanceOf[PlayerTank] => element
 
 
-    override def step(delta: Double): this.type = ???
-    /*
-        TODO: add this to main powerup controller time step
-        powerUpBinder.getPowerUps.collect:
-        case element: TimeablePowerUp =>
-            element.decreaseDuration(deltaTime)
-            if element.isExpired then
-                powerUpBinder.unchain(element)
-        this
+    /**
+     * Updates the state of the power-up controller based on the elapsed time.
+     * The elapsed time is detracted from the `timeToDisappaer` total time
+     *
+     * @param delta The elapsed time since the last update.
+     * @return The updated power-up controller.
      */
+    override def step(delta: Double): this.type =
+        timeToDisappear -= delta
+        if timeToDisappear <= 0.0 then
+            entityRepo.removeModelView(powerUp)
+        this
+
+object EnemySpawnController:
+    def factory[E](viewScale: Double, entityRepo: EntityMvRepositoryContainer[AnyRef, Node])(context: EntityRepositoryContext[Stage, ?, ?], powerUp: PickablePowerUp[E], view: PowerUpView) =
+        new PowerUpController(using context)(entityRepo, powerUp, view, viewScale)
+
 
 
 
