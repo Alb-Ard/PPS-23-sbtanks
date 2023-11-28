8589ca9f99c33710a8f6a26be2d3c7b8e9467c40
Andrea Bianchi <andrea.bianchi24@studio.unibo.it>
2023-11-28 18:32:56 +0100

    slight fixes in LevelLoader

--- src/main/scala/org/aas/sbtanks/level/LevelLoader.scala
+++ src/main/scala/org/aas/sbtanks/level/LevelLoader.scala
@@ -6,10 +6,10 @@ import scala.collection.mutable.ListBuffer
 import scala.io.Source.*
 
 class LevelLoader: //da qui leggerò i file txt (guarda le librerie che si occupano di caricare tali file
-    def getLevelSeq(count: Int, offset: Int = 0): (Seq[(String, Int, Int)], Seq[String]) =
+    def getLevelSeq(amount: Int): (Seq[(String, Int, Int)], Seq[String]) =
         val levels = new ListBuffer[(String, Int, Int)]
         val enemySequencePerLevel = new ListBuffer[String]
-        for(n <- Range.inclusive(offset + 1, count))
+        for(n <- Range.inclusive(1, amount))
             levels.addOne(getLevel(n)._1, 11, getLevel(n)._2.size)
             enemySequencePerLevel += getLevel(n)._2
         (levels.toSeq, enemySequencePerLevel.toSeq)
